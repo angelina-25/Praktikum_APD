73a66b7891a6b1aefff8d40d95ef3b7683ec1b36
angelina-25 <augustinumami46@gmail.com>
2024-09-18 20:33:03 +0800

    Add files via upload

---
+++ Praktikum-APD/kelas A1'24 informatika/Pertemuan 3/pertemuan 3.py
@@ -0,0 +1,11 @@
+angka = int(input("Masukkan angka: "))
+
+if angka > 30:
+    print ("sapi")
+    if angka > 50:
+        print("singa")
+        if angka = 60:
+
+else:
+    print("invalid")        
+            

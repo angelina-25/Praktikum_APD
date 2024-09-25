f546a6158a780b13ed5d5829e75ade5eca606cba
angelina-25 <augustinumami46@gmail.com>
2024-09-25 09:17:03 +0800

    Add files via upload

---
+++ Praktikum-APD/kelas A1'24 informatika/Pertemuan 4.py
@@ -0,0 +1,62 @@
+# ulang = 10
+# for i in range(ulang):
+#   print(f"Perulangan ke-{str(i)}")
+
+# simpan = [12, "udin petot", 14.5, True, 'A']
+# for i in simpan:
+#    print(i, end=' ')
+
+#for i in range(1, 10, 2): #(awal, akhir, lompatan
+#    print(i))
+
+#for i in range(1, 4):
+#     for j in range(1, 4):
+#          print(f"{i} x {j} = {i * j}")
+#     print()
+
+#a = 10
+#b = 7
+
+#print(f"nilai a : {a} dan nilai b : {b}")
+
+#print("nilai a : " + str{a})
+
+#jawab = 'ya'
+#hitung = 0
+
+#while(jawab == 'ya'): #kondisi/syarat
+#    hitung+= 2 #hitung =hitung + 2
+#    jawab = input("hitung lagi tidak? : ")
+#print(f"total perulangan : {hitung}")
+
+#jawab = 'ya'
+
+#hitung = 0
+
+#while(jawab == 'ya'):
+#    hitung += 1
+#    ulang = input("Ulang lagi tidak? : ")
+#    if ulang == "tidak" or ulang =="Tidak":
+#       break
+
+#print(f"total perulangan : {hitung}")
+
+#print("daftar bilangan ganjil dari 1-10")
+#for item in range(10):
+#    if item % 3 == 0:
+#        continue
+#    print(item)
+
+#bilangan = 0
+#while True:
+#    angka = int(input("Masukkan angka: "))
+#    if angka < 0:
+#        break
+#    bilangan += angka
+
+#print("Total bilangan: " + str(bilangan)) 
+
+# for i in range(1, 20, 3):
+#    if i % 2 == 0:
+#        continue
+#   print(i)
